ce70e5eac3c645d6eb77a6d6d3f89f9ba62e2882
Luca Carducci <luca.carducci91@gmail.com>
2019-09-06 16:40:04 +0200

    print label

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,43 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { TreeProvider } from './treeProvider';
+import { TreeProvider } from "./treeProvider";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(data: vscode.ExtensionContext) {
+  let skObject = [
+    {
+      name: "Film",
+      pos: 34,
+      _attr: [
+        {
+          name: "title",
+          pos: 37
+        },
+        {
+          name: "year",
+          pos: 40
+        }
+      ]
+    },
+    {
+      name: "Actor",
+      pos: 45,
+      _attr: []
+    }
+  ];
 
-	var data = [
-		{
-			name: "Film",
-			pos: 34,
-			_attr: [
-				{
-					name: "title",
-					pos: 37
-				},
-				{
-					name: "year",
-					pos: 40
-				}
-			]
-		},
-		{
-			name: "Actor",
-			pos: 45,
-			_attr: [
-			]
-		}
-	];
-  
-  const jsonOutlineProvider = new TreeProvider(data);
+  const jsonOutlineProvider = new TreeProvider(skObject);
 
-	vscode.window.registerTreeDataProvider('skaffolderExplorer', jsonOutlineProvider);
-	// vscode.commands.registerCommand('jsonOutline.refresh', () => jsonOutlineProvider.refresh());
-	// vscode.commands.registerCommand('jsonOutline.refreshNode', offset => jsonOutlineProvider.refresh(offset));
-	// // vscode.commands.registerCommand('jsonOutline.renameNode', offset => jsonOutlineProvider.rename(offset));
-	// vscode.commands.registerCommand('extension.openJsonSelection', range => jsonOutlineProvider.select(range));
-	
+  vscode.window.registerTreeDataProvider(
+    "skaffolderExplorer",
+    jsonOutlineProvider
+  );
+  // vscode.commands.registerCommand('jsonOutline.refresh', () => jsonOutlineProvider.refresh());
+  // vscode.commands.registerCommand('jsonOutline.refreshNode', offset => jsonOutlineProvider.refresh(offset));
+  // // vscode.commands.registerCommand('jsonOutline.renameNode', offset => jsonOutlineProvider.rename(offset));
+  // vscode.commands.registerCommand('extension.openJsonSelection', range => jsonOutlineProvider.select(range));
 }
 // this method is called when your extension is deactivated
 export function deactivate() {}
--- src/treeProvider.ts
+++ src/treeProvider.ts
@@ -1,40 +1,71 @@
-import * as vscode from 'vscode';
-import * as json from 'jsonc-parser';
+import * as vscode from "vscode";
+import * as json from "jsonc-parser";
 
 export class TreeProvider implements vscode.TreeDataProvider<number> {
-    
-    private _onDidChangeTreeData: vscode.EventEmitter<number | null> = new vscode.EventEmitter<number | null>();
-    
-    
-    private autoRefresh: boolean = true;
+  private _onDidChangeTreeData: vscode.EventEmitter<
+    number | null
+  > = new vscode.EventEmitter<number | null>();
 
-    constructor(data: vscode.ExtensionContext) {
-        console.log('init provider');
-        console.log(data);
-        this.autoRefresh = vscode.workspace.getConfiguration('skaffolderExplorer').get('autorefresh');
-        vscode.workspace.onDidChangeConfiguration(()=> {
-            this.autoRefresh = vscode.workspace.getConfiguration('skaffolderExplorer').get('autorefresh');
-        });
-        this.onActiveEditorChange();
+  private autoRefresh: boolean = true;
+
+  constructor(data: vscode.ExtensionContext) {
+    console.log("init provider");
+    console.log(data);
+    this.autoRefresh = vscode.workspace
+      .getConfiguration("skaffolderExplorer")
+      .get("autorefresh");
+    vscode.workspace.onDidChangeConfiguration(() => {
+      this.autoRefresh = vscode.workspace
+        .getConfiguration("skaffolderExplorer")
+        .get("autorefresh");
+    });
+    this.onActiveEditorChange();
+  }
+
+  private onActiveEditorChange(): void {
+    if (vscode.window.activeTextEditor) {
+      if (vscode.window.activeTextEditor.document.uri.scheme === "file") {
+        const enabled =
+          vscode.window.activeTextEditor.document.languageId === "json" ||
+          vscode.window.activeTextEditor.document.languageId === "jsonc";
+        vscode.commands.executeCommand(
+          "setContext",
+          "jsonOutlineEnabled",
+          enabled
+        );
+      }
+    } else {
+      vscode.commands.executeCommand("setContext", "jsonOutlineEnabled", false);
     }
+  }
 
-    private onActiveEditorChange(): void {
-        if(vscode.window.activeTextEditor) {
-            if(vscode.window.activeTextEditor.document.uri.scheme === 'file') {
-                const enabled = vscode.window.activeTextEditor.document.languageId === 'json' || vscode.window.activeTextEditor.document.languageId === 'jsonc';
-                vscode.commands.executeCommand('setContext', 'jsonOutlineEnabled', enabled);
-            }
-        } else {
-            vscode.commands.executeCommand('setContext', 'jsonOutlineEnabled', false);
-        }
-    }  
-    
-    getTreeItem(element: number): vscode.TreeItem | Thenable<vscode.TreeItem> {
-        throw new Error("Method not implemented.");
-    }
-    getChildren(element?: number | undefined): vscode.ProviderResult<number[]> {
-        throw new Error("Method not implemented.");
-    }
+  getTreeItem(element: number): vscode.TreeItem | Thenable<vscode.TreeItem> {
+    // throw new Error("Method not implemented.");
+    console.log(" Get tree item ", element);
 
+    let myItem: vscode.TreeItem = new vscode.TreeItem(
+      "Label",
+      vscode.TreeItemCollapsibleState.None
+    );
+    // myItem.command = {
+    //   command: "extension.openJsonSelection",
+    //   title: "",
+    //   arguments: [
+    //     new vscode.Range(
+    //       this.editor.document.positionAt(valueNode.offset),
+    //       this.editor.document.positionAt(valueNode.offset + valueNode.length)
+    //     )
+    //   ]
+    // };
+    // myItem.iconPath = this.getIcon(valueNode);
+    // myItem.contextValue = valueNode.type;
+    return myItem;
+  }
+  getChildren(element?: number | undefined): vscode.ProviderResult<number[]> {
+    console.log(" Get children ", element);
+    // throw new Error("Method not implemented.");
+    const offsets: number[] = [1, 3];
 
+    return offsets;
+  }
 }
