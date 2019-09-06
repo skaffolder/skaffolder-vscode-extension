4b4c302d110e4046323ec2775871c03b77e4bb23
Luca Carducci <luca.carducci91@gmail.com>
2019-09-06 17:00:19 +0200

    show name resource

--- src/treeProvider.ts
+++ src/treeProvider.ts
@@ -1,13 +1,6 @@
 import * as vscode from "vscode";
-import * as json from "jsonc-parser";
 
 export class TreeProvider implements vscode.TreeDataProvider<number> {
-  private _onDidChangeTreeData: vscode.EventEmitter<
-    number | null
-  > = new vscode.EventEmitter<number | null>();
-
-  private autoRefresh: boolean = true;
-
   private skObject = [
     {
       name: "Film",
@@ -32,61 +25,43 @@ export class TreeProvider implements vscode.TreeDataProvider<number> {
 
   constructor(data: vscode.ExtensionContext) {
     console.log("init provider");
-    console.log(data);
-    // this.autoRefresh = vscode.workspace
-    //   .getConfiguration("skaffolderExplorer")
-    //   .get("autorefresh");
-    // vscode.workspace.onDidChangeConfiguration(() => {
-    //   this.autoRefresh = vscode.workspace
-    //     .getConfiguration("skaffolderExplorer")
-    //     .get("autorefresh");
-    // });
-    this.onActiveEditorChange();
+    vscode.commands.executeCommand("setContext", "jsonOutlineEnabled", true);
+
+    // this.onActiveEditorChange();
   }
 
-  private onActiveEditorChange(): void {
-    if (vscode.window.activeTextEditor) {
-      if (vscode.window.activeTextEditor.document.uri.scheme === "file") {
-        const enabled =
-          vscode.window.activeTextEditor.document.languageId === "json" ||
-          vscode.window.activeTextEditor.document.languageId === "jsonc";
-        vscode.commands.executeCommand(
-          "setContext",
-          "jsonOutlineEnabled",
-          enabled
-        );
-      }
-    } else {
-      vscode.commands.executeCommand("setContext", "jsonOutlineEnabled", false);
-    }
-  }
+  // private onActiveEditorChange(): void {
+  //   if (vscode.window.activeTextEditor) {
+  //     if (vscode.window.activeTextEditor.document.uri.scheme === "file") {
+  //       const enabled =
+  //         vscode.window.activeTextEditor.document.languageId === "json" ||
+  //         vscode.window.activeTextEditor.document.languageId === "jsonc";
+  //       vscode.commands.executeCommand(
+  //         "setContext",
+  //         "jsonOutlineEnabled",
+  //         enabled
+  //       );
+  //     }
+  //   } else {
+  //     vscode.commands.executeCommand("setContext", "jsonOutlineEnabled", false);
+  //   }
+  // }
 
   getTreeItem(element: number): vscode.TreeItem | Thenable<vscode.TreeItem> {
-    // throw new Error("Method not implemented.");
     console.log(" Get tree item ", element);
 
     let myItem: vscode.TreeItem = new vscode.TreeItem(
-      "Label",
+      this.skObject[element - 1].name,
       vscode.TreeItemCollapsibleState.None
     );
-    // myItem.command = {
-    //   command: "extension.openJsonSelection",
-    //   title: "",
-    //   arguments: [
-    //     new vscode.Range(
-    //       this.editor.document.positionAt(valueNode.offset),
-    //       this.editor.document.positionAt(valueNode.offset + valueNode.length)
-    //     )
-    //   ]
-    // };
-    // myItem.iconPath = this.getIcon(valueNode);
-    // myItem.contextValue = valueNode.type;
     return myItem;
   }
   getChildren(element?: number | undefined): vscode.ProviderResult<number[]> {
-    console.log(" Get children ", element);
-    // throw new Error("Method not implemented.");
-    const offsets: number[] = [1, 3];
+    console.log("Get children ");
+    const offsets: number[] = this.skObject.map((item, count) => {
+      return count + 1;
+    });
+    console.log(offsets);
 
     return offsets;
   }
