97dbbda5e95859e47885dc41c17c6b1f13c072cf
Luca Carducci <luca.carducci91@gmail.com>
2019-09-07 22:32:54 +0200

    add page

--- src/models/SkaffolderObject.ts
+++ src/models/SkaffolderObject.ts
@@ -1,9 +1,11 @@
 import { Project } from "./jsonreader/project";
 import { Db } from "./jsonreader/db";
+import { Page } from "./jsonreader/page";
 
 export class SkaffolderObject {
   constructor() {}
 
   public project?: Project;
   public resources: Db[] = [];
+  public modules: Page[] = [];
 }
