62ecf5d698d4067da1faa196ae936ee0a3644bfd
Luca Carducci <luca.carducci91@gmail.com>
2019-09-08 02:09:45 +0200

    read yaml

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -1,9 +1,11 @@
 import * as fs from "fs";
 import { SkaffolderObject } from "../models/skaffolderObject";
 import { Db } from "../models/jsonreader/db";
+import * as yaml from "yaml";
 
 export class DataService {
   private dataObj: SkaffolderObject;
+  private fileObj: SkaffolderObject;
 
   constructor() {
     let data = fs.readFileSync(
@@ -11,6 +13,12 @@ export class DataService {
       "utf-8"
     );
     this.dataObj = JSON.parse(data);
+
+    let dataYaml = fs.readFileSync(
+      __dirname + "/../../src/services/dataExampleOpenAPI3.yaml",
+      "utf-8"
+    );
+    this.fileObj = yaml.parse(dataYaml);
   }
 
   public getSkObject() {
