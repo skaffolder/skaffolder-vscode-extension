883c39bb524821bc9ea484e6091a9559db23f9e8
Luca Carducci <luca.carducci91@gmail.com>
2020-01-02 15:25:23 +0100

    goto sk env

--- src/commands/GoToSkaffolderCommand.ts
+++ src/commands/GoToSkaffolderCommand.ts
@@ -1,12 +1,14 @@
 import { SkaffolderNode } from "../models/SkaffolderNode";
 import { DataService } from "../services/DataService";
+import * as SkaffolderCli from "skaffolder-cli";
 const opn = require("opn");
 
 export class GoToSkaffolderCommand {
   static async command(context: SkaffolderNode) {
     const obj = DataService.getSkObject();
     let projectId = obj.project ? obj.project._id : "";
-    let url = `https://app.skaffolder.com/#!/projects/${projectId}/models`;
+
+    let url = `${SkaffolderCli.getEnv()}/#!/projects/${projectId}/models`;
     opn(url, {
       wait: false
     });
