0a83529cd3c3389783ed24584b29612b5233f4e5
Luca Carducci <luca.carducci91@gmail.com>
2020-03-03 17:57:10 -0600

    show error yaml parsing

--- src/services/DataService.ts
+++ src/services/DataService.ts
@@ -162,8 +162,43 @@ export class DataService {
       } catch (e) {
         console.error('File "openapi.yaml" not parsable');
         console.error(e);
-        vscode.window.showErrorMessage(e.message);
-        vscode.window.showErrorMessage('File "openapi.yaml" not parsable');
+        vscode.window.showErrorMessage("Line: " + e.source.rawValue);
+        vscode.window.showErrorMessage(
+          "ERROR at line " + e.source.rangeAsLinePos.start.line + ", col " + e.source.rangeAsLinePos.start.col
+        );
+        vscode.window.showErrorMessage('File "openapi.yaml" not parsable: ' + e.message);
+
+        // Open yaml file
+
+        let configFilePath = vscode.Uri.file(vscode.workspace.rootPath + "/openapi.yaml");
+        // Open file openapi
+        try {
+          let docProm = vscode.workspace.openTextDocument(configFilePath);
+          docProm.then(doc => {
+            let tab = vscode.window.showTextDocument(doc, vscode.ViewColumn.One);
+
+            var range: vscode.Range = new vscode.Range(
+              e.source.rangeAsLinePos.start.line - 1,
+              e.source.rangeAsLinePos.start.col - 1,
+              e.source.rangeAsLinePos.end.line - 1,
+              e.source.rangeAsLinePos.end.col - 1
+            );
+            tab.then(tab => {
+              // Select range
+              let selection: vscode.Selection = new vscode.Selection(range.start, range.end);
+              tab.selection = selection;
+
+              // Scroll to pos
+              let posStart: vscode.Position = new vscode.Position(range.start.line - 1, 0);
+              let posEnd: vscode.Position = new vscode.Position(range.end.line + 100, 0);
+              let rangeScroll: vscode.Range = new vscode.Range(posStart, posEnd);
+              tab.revealRange(rangeScroll);
+            });
+          });
+        } catch (e) {
+          console.error(e);
+        }
+
         throw e;
       }
 
